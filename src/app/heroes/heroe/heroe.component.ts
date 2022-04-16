d872ff490cc457abcdb0223707270bd6e27a23eb
Alejandra Rodriguez <jacfalcom@gmail.com>
2022-04-16 15:56:20 -0600

    seccion1 to seccion5

---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,23 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-heroe',
+    templateUrl: 'heroe.component.html'
+})
+
+export class HeroeComponent{
+    nombre:string = 'martin';
+    edad:number = 35;
+    get nombreCapitalizado():string{
+        return this.nombre.toUpperCase();
+    }
+    obtenerNombre():string{
+        return `${this.nombre} - ${this.edad}`
+    }
+    cambiarNombre():void{
+        this.nombre = 'alejandra';
+    }
+    cambiarEdad():void{
+        this.edad = 50;
+    }
+}
